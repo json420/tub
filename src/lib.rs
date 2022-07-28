bdad99654e019c70d56d186d91e2b4b2a668416a
Jason Gerard DeRose <jasonderose@gmail.com>
2022-07-27 21:33:03 -0600

    Doodles with blake2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,54 @@
+use blake2::{Blake2b, Digest};
+use digest::consts::{U30,U10};
+use generic_array::GenericArray;
+
+
+type Blake2b80 = Blake2b<U10>;
+type Blake2b240 = Blake2b<U30>;
+
+
+fn hash80(buf: &[u8]) -> GenericArray<u8, U10> {
+    let mut h = Blake2b80::new();
+    h.update(buf);
+    h.finalize()
+}
+
+
+fn hash240(buf: &[u8]) -> GenericArray<u8, U30> {
+    let mut h = Blake2b240::new();
+    h.update(buf);
+    h.finalize()
+}
+
+
 #[cfg(test)]
 mod tests {
+    use hex_literal::hex;
+    use super::*;
+
+    static d1: &[u8] = b"my_input";
+    static d1h80: [u8; 10] = hex!("2cc55c84e416924e6400");
+    static d1h240: [u8; 30] = hex!("35f6b8fe184790c47717de56324629309370b1f37b1be1736027d414c122");
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_hash80() {
+        let mut h = Blake2b80::new();
+        h.update(d1);
+        let res = h.finalize();
+        assert_eq!(res[..], (d1h80[..])[..]);
+
+        let res = hash80(d1);
+        assert_eq!(res[..], d1h80[..]);
+    }
+
+    #[test]
+    fn test_hash240() {
+        let mut h = Blake2b240::new();
+        h.update(d1);
+        let res = h.finalize();
+        assert_eq!(res[..], (d1h240[..])[..]);
+
+        let res = hash240(d1);
+        assert_eq!(res[..], d1h240[..]);
     }
 }

9c0106346fd6981c175a83df1f7dbda947f5b52f
Jason Gerard DeRose <jasonderose@gmail.com>
2022-10-24 19:49:40 -0600

    Add benchmark for dbase32::decode()

--- benches/mybench.rs
+++ benches/mybench.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use bathtub_db::util::*;
 use bathtub_db::protocol::hash;
-use bathtub_db::dbase32::db32enc_into;
+use bathtub_db::dbase32::{db32enc, db32enc_into, decode};
 use bathtub_db::base::*;
 
 
@@ -22,10 +22,17 @@ fn bm_db32enc_into(c: &mut Criterion) {
     );
 }
 
+fn bm_decode(c: &mut Criterion) {
+    let txt = db32enc(&random_object_id());
+    c.bench_function("decode",
+        |b| b.iter(|| decode(black_box(&txt)))
+    );
+}
+
 criterion_group!{
     name = benches;
     config = Criterion::default();
-    targets = bm_random_id, bm_hash, bm_db32enc_into
+    targets = bm_random_id, bm_hash, bm_db32enc_into, bm_decode
 }
 
 criterion_main!(benches);

8426f849eb9cbf1ef2e7a1d84ca06561a514d719
Jason Gerard DeRose <jasonderose@gmail.com>
2022-11-05 06:10:32 -0600

    Rename ScanResult to Scanner; flesh out tests for the same

--- src/importer.rs
+++ src/importer.rs
@@ -1,48 +1,59 @@
 use std::path::{Path, PathBuf};
-use std::fs::read_dir;
+use std::fs::{read_dir, metadata, File};
 
 
 const MAX_DEPTH: usize = 32;
 
-fn scan_files<P: AsRef<Path>>(dir: P, accum: &mut Vec<PathBuf>, depth: usize) {
-    assert!(depth <= MAX_DEPTH + 1);
-    if depth > MAX_DEPTH {
-        return;
-    }
-    if let Ok(entries) = read_dir(dir) {
-        for entry in entries {
-            let e = entry.unwrap();
-            let ft = e.file_type().unwrap();
-            let path = e.path();
-            if ft.is_file() {
-                accum.push(path);
-            }
-            else if ft.is_dir() {
-                scan_files(path, accum, depth + 1);
+pub struct SrcFile(pub PathBuf, pub u64);
+
+fn scan_files<P: AsRef<Path>>(dir: P, accum: &mut Vec<SrcFile>, depth: usize) -> u64 {
+    if depth < MAX_DEPTH {
+        let mut total: u64 = 0;
+        if let Ok(entries) = read_dir(dir) {
+            for entry in entries {
+                let e = entry.unwrap();
+                let ft = e.file_type().unwrap();
+                let path = e.path();
+                if ft.is_file() {
+                    let size = metadata(&path).unwrap().len();
+                    accum.push(SrcFile(path, size));
+                    total += size;
+                }
+                else if ft.is_dir() {
+                    total += scan_files(path, accum, depth + 1);
+                }
             }
         }
+        total
+    }
+    else {
+        0
     }
 }
 
 
-pub struct ScanResult {
-    accum: Vec<PathBuf>,
+pub struct Scanner {
+    accum: Vec<SrcFile>,
+    total: u64,
 }
 
-impl ScanResult {
-    fn scan_dir<P: AsRef<Path>>(dir: P) -> Self {
-        let mut accum: Vec<PathBuf> = Vec::new();
-        scan_files(dir, &mut accum, 0);
-        accum.sort();
-        ScanResult {accum: accum}
+impl Scanner {
+    pub fn scan_dir<P: AsRef<Path>>(dir: P) -> Self {
+        let mut accum: Vec<SrcFile> = Vec::new();
+        let total = scan_files(dir, &mut accum, 0);
+        Scanner {accum: accum, total: total}
     }
 
-    fn iter(&self) -> std::slice::Iter<PathBuf> {
+    pub fn iter(&self) -> std::slice::Iter<SrcFile> {
         self.accum.iter()
     }
 
-    fn len(&self) -> usize {
+    pub fn count(&self) -> usize {
         self.accum.len()
+    }
+
+    pub fn total(&self) -> u64 {
+        self.total
     }
 }
 
@@ -50,11 +61,66 @@ impl ScanResult {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempfile::TempDir;
+    use std::fs::{create_dir_all, File};
+    use std::io::prelude::*;
+
+    fn create_test_dirs(tmp: &TempDir) {
+        create_dir_all(tmp.path().join("A").join("B").join("C"));
+    }
+
+    fn create_test_files(tmp: &TempDir) {
+        File::create(
+            tmp.path().join("Z")
+        ).unwrap().write_all(&[0_u8; 7]);
+        File::create(
+            tmp.path().join("A").join("Y")
+        ).unwrap().write_all(&[1_u8; 11]);
+    }
+
 
     #[test]
     fn test_scan_files() {
-        let mut accum: Vec<PathBuf> = Vec::new();
-        scan_files("/no/such/dir", &mut accum, MAX_DEPTH + 1);
+        let tmp = TempDir::new().unwrap();
+
+        let mut accum: Vec<SrcFile> = Vec::new();
+        scan_files(tmp.path(), &mut accum, MAX_DEPTH + 1);
         assert_eq!(accum.len(), 0);
+
+        // Contains directories but no files
+        create_test_dirs(&tmp);
+        assert_eq!(scan_files(tmp.path(), &mut accum, 0), 0);
+        assert_eq!(accum.len(), 0);
+
+        // Contains files but called at MAX_DEPTH
+        create_test_files(&tmp);
+        assert_eq!(scan_files(tmp.path(), &mut accum, MAX_DEPTH), 0);
+        assert_eq!(accum.len(), 0);
+
+        // All good, should find test files
+        assert_eq!(scan_files(tmp.path(), &mut accum, 0), 18);
+        assert_eq!(accum.len(), 2);
+    }
+
+    #[test]
+    fn test_scanresult() {
+        let tmp = TempDir::new().unwrap();
+
+        // Empty directory
+        let s = Scanner::scan_dir(tmp.path());
+        assert_eq!(s.count(), 0);
+        assert_eq!(s.total(), 0);
+
+        // Contains directories but no files
+        create_test_dirs(&tmp);
+        let s = Scanner::scan_dir(tmp.path());
+        assert_eq!(s.count(), 0);
+        assert_eq!(s.total(), 0);
+
+        // Contains files
+        create_test_files(&tmp);
+        let s = Scanner::scan_dir(tmp.path());
+        assert_eq!(s.count(), 2);
+        assert_eq!(s.total(), 18);
     }
 }

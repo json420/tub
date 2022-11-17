6309b2c25cc441b64548cfd6d89e84c54a95fa21
Jason Gerard DeRose <jasonderose@gmail.com>
2022-11-16 19:14:46 -0700

    Ok, I like the clap derive approach better

--- src/bin/tub.rs
+++ src/bin/tub.rs
@@ -1,44 +1,6 @@
-//use bathtub_db::commands::{get_args, run};
-
-use std::ffi::OsString;
-use std::path::PathBuf;
-
-use clap::{arg, Command};
-
-
-fn cli() -> Command
-{
-    Command::new("tub")
-        .about("Super weird and awesome DVCS")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .allow_external_subcommands(true)
-        .subcommand(
-            Command::new("init")
-                .about("Initialized a BathtubDB repository")
-        )
-        .subcommand(
-            Command::new("commit")
-                .about("Commit stuff and junk")
-        )
-        .subcommand(
-            Command::new("import")
-                .about("Import files into object store")
-        )
-        
-}
-
+use bathtub_db::commands::run;
 
 
 fn main() {
-    let matches = cli().get_matches();
-
-    match matches.subcommand() {
-        Some(("init", sub_matches)) => {
-            println!("running init");
-        }
-        
-        
-        _ => unreachable!(),
-    }
+    run();
 }
--- src/commands.rs
+++ src/commands.rs
@@ -1,116 +1,35 @@
+use std::ffi::{OsString, OsStr};
+use std::path::PathBuf;
 
-use std::collections::{BTreeMap, VecDeque};
-use std::env;
+use clap::{Args, Parser, Subcommand, ValueEnum};
 
 
-pub type CmdArgs = VecDeque<String>;
-
-/// Collect `env::args()` into a `VecDeque`.
-pub fn get_args() -> CmdArgs
-{
-    let mut args: CmdArgs = env::args().collect();
-    args.pop_front().unwrap();
-    args
+#[derive(Debug, Parser)]
+#[command(name = "tub")]
+#[command(about = "The most kickass DVCS of all?")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
 
 
-trait Command {
-    fn name(&self) -> String;
-    fn run(&self, args: &CmdArgs);
-}
+#[derive(Debug, Subcommand)]
+enum Commands {
 
-pub type CmdType = Box<dyn Command>;
-
-
-struct InitCmd {
-
-}
-impl Command for InitCmd {
-    fn name(&self) -> String {
-        "init".to_string()
-    }
-
-    fn run(&self, args: &CmdArgs) {
-        println!("runnig init");
-    }
-}
-
-struct ImportCmd {
-
-}
-impl Command for ImportCmd {
-    fn name(&self) -> String {
-        "import".to_string()
-    }
-
-    fn run(&self, args: &CmdArgs) {
-        println!("runnig import");
+    #[command(about = "Initalize a Bathtub DB repository")]
+    Init {
+        #[arg(help = "Target directory (defaults to current working directory)")]
+        target: Option<PathBuf>,
     }
 }
 
 
-struct Dispatcher {
-    map: BTreeMap<String, CmdType>,
-}
 
-impl Dispatcher {
-    fn new() -> Self {
-        Self {
-            map: BTreeMap::new(),
+pub fn run() {
+    let args = Cli::parse();
+    match args.command {
+        Commands::Init { target } => {
+            println!("init {:?}", target);
         }
     }
-
-    fn add(&mut self, cmd: CmdType) {
-        self.map.insert(cmd.name(), cmd);
-    }
-
-    fn run(&self, args: &mut CmdArgs) {
-        if let Some(name) = args.pop_front() {
-            if let Some(cmd) = self.map.get(&name) {
-                cmd.run(args);
-            }
-            else {
-                eprintln!("Unknown command: {:?}", name);
-            }
-        }
-        else {
-            eprintln!("Available commands:");
-            for cmd in self.map.values() {
-                eprintln!("  {}", cmd.name());
-            }
-        }
-    }
-
 }
-
-fn build_dispatcher() -> Dispatcher {
-    let mut d = Dispatcher::new();
-    d.add(Box::new( ImportCmd {} ));
-    d.add(Box::new( InitCmd {} ));
-    //d.add(Box::new( ImportCmd {} ));
-    d
-}
-
-
-
-pub fn run(args: &mut CmdArgs) {
-    let dispatcher = build_dispatcher();
-    dispatcher.run(args);
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_args() {
-        get_args();
-    }
-
-    #[test]
-    fn test_dispatcher() {
-        let mut d = Dispatcher::new();
-    }
-}
-

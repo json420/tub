4b4ef7fac68cb8c14bbc0bc90dfab78280ae6154
Jason Gerard DeRose <jasonderose@gmail.com>
2022-11-18 17:07:22 -0700

    hash_leaf(), hash_root() now return LeafInfo, RootInfo structs

--- src/protocol.rs
+++ src/protocol.rs
@@ -22,17 +22,30 @@ pub fn hash(buf: &[u8]) -> ObjectID {
 }
 
 
-pub fn hash_leaf(index: LeafIndex, data: &[u8]) -> LeafHash {
+#[derive(Debug, PartialEq)]
+pub struct LeafInfo {
+    pub index: u64,
+    pub hash: LeafHash,
+}
+
+pub fn hash_leaf(index: u64, data: &[u8]) -> LeafInfo {
     let mut h = blake3::Hasher::new();
     h.update(&index.to_le_bytes());
     h.update(data);
-    let mut lh: LeafHash = [0_u8; LEAF_HASH_LEN];
-    h.finalize_xof().fill(&mut lh);
-    lh
+    let mut hash: LeafHash = [0_u8; LEAF_HASH_LEN];
+    h.finalize_xof().fill(&mut hash);
+    LeafInfo {index: index, hash: hash}
 }
 
 
-pub fn hash_root(size: ObjectSize, leaf_hashes: LeafHashList) -> ObjectID {
+#[derive(Debug, PartialEq)]
+pub struct RootInfo {
+    pub size: u64,
+    pub hash: LeafHash,
+    pub leaf_hashes: LeafHashList,
+}
+
+pub fn hash_root(size: u64, leaf_hashes: LeafHashList) -> RootInfo {
     let mut h = blake3::Hasher::new();
     h.update(&size.to_le_bytes());
     for leaf_hash in leaf_hashes.iter() {
@@ -40,7 +53,7 @@ pub fn hash_root(size: ObjectSize, leaf_hashes: LeafHashList) -> ObjectID {
     }
     let mut id: ObjectID = [0_u8; OBJECT_ID_LEN];
     h.finalize_xof().fill(&mut id);
-    id
+    RootInfo {size: size, hash: id, leaf_hashes: leaf_hashes}
 }
 
 
@@ -63,7 +76,9 @@ mod tests {
         let obj = random_small_object();
         let lh0 = hash_leaf(0, &obj);
         let lh1 = hash_leaf(1, &obj);
-        assert_ne!(lh0, lh1);
+        assert_eq!(lh0.index, 0);
+        assert_eq!(lh1.index, 1);
+        assert_ne!(lh0.hash, lh1.hash);
     }
 
     #[test]
@@ -72,6 +87,11 @@ mod tests {
         let lh1 = random_object_id();
         let a = hash_root(1, vec![lh0]);
         let b = hash_root(LEAF_SIZE, vec![lh0]);
-        assert_ne!(a, b);
+        let c = hash_root(LEAF_SIZE + 1, vec![lh0, lh1]);
+        let d = hash_root(2 * LEAF_SIZE, vec![lh0, lh1]);
+        assert_eq!(a.leaf_hashes, b.leaf_hashes);
+        assert_ne!(a.hash, b.hash);
+        assert_eq!(c.leaf_hashes, d.leaf_hashes);
+        assert_ne!(c.hash, d.hash);
     }
 }

1d0a97f8def024d604d72c686e2033b14141b942
Jason Gerard DeRose <jasonderose@gmail.com>
2022-12-04 14:04:25 -0700

    Stub out new zero-copy Header to be used by TubBuf and ReindexBuf

--- src/leaf_io.rs
+++ src/leaf_io.rs
@@ -527,6 +527,19 @@ impl TubBuf {
     }
 }
 
+
+pub struct Header<'a> {
+    buf: &'a [u8],
+}
+
+impl<'a> Header<'a>
+{
+    pub fn new(buf: &'a [u8]) ->  Self {
+        Self {buf: buf}
+    }
+}
+
+
 pub struct ReindexBuf {
     buf: Vec<u8>,
 }

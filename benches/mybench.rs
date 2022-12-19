07043b523437e3bab456e66007794d47c850e95f
Jason Gerard DeRose <jasonderose@gmail.com>
2022-12-19 13:59:47 -0700

    Add a blake2b benchmark

--- benches/mybench.rs
+++ benches/mybench.rs
@@ -4,13 +4,23 @@ use bathtub_db::util::*;
 use bathtub_db::dbase32::{db32enc, isdb32, db32enc_into, db32dec_into};
 use bathtub_db::base::*;
 
+use blake2::{Blake2b, Digest, digest::consts::U30};
 
-pub fn hash(data: &[u8]) -> TubHash {
+type Blake2b240 = Blake2b<U30>;
+
+
+pub fn hash_blake3(data: &[u8]) -> TubHash {
     let mut h = blake3::Hasher::new();
     h.update(data);
     let mut hash: TubHash = [0_u8; TUB_HASH_LEN];
     h.finalize_xof().fill(&mut hash);
     hash
+}
+
+pub fn hash_blake2b(data: &[u8]) {
+    let mut hasher = Blake2b240::new();
+    hasher.update(data);
+    hasher.finalize();
 }
 
 
@@ -22,14 +32,27 @@ fn bm_random_id(c: &mut Criterion) {
 fn bm_hash(c: &mut Criterion) {
     let mut buf = vec![0_u8; 4096];
     getrandom(&mut buf[..]);
-    c.bench_function("hash 4 KiB", |b| b.iter(|| hash(black_box(&buf[..]))));
+    c.bench_function("blake3 4 KiB", |b| b.iter(|| hash_blake3(black_box(&buf[..]))));
 }
 
 fn bm_hash2(c: &mut Criterion) {
     let mut buf = vec![0_u8; 65536];
     getrandom(&mut buf[..]);
-    c.bench_function("hash 64 KiB", |b| b.iter(|| hash(black_box(&buf[..]))));
+    c.bench_function("blake3 64 KiB", |b| b.iter(|| hash_blake3(black_box(&buf[..]))));
 }
+
+fn bm_blake2b(c: &mut Criterion) {
+    let mut buf = vec![0_u8; 4096];
+    getrandom(&mut buf[..]);
+    c.bench_function("blake2b 4 KiB", |b| b.iter(|| hash_blake2b(black_box(&buf[..]))));
+}
+
+fn bm_blake2b_64k(c: &mut Criterion) {
+    let mut buf = vec![0_u8; 65536];
+    getrandom(&mut buf[..]);
+    c.bench_function("blake2b 64 KiB", |b| b.iter(|| hash_blake2b(black_box(&buf[..]))));
+}
+
 
 fn bm_isdb32(c: &mut Criterion) {
     let txt = db32enc(&random_hash());
@@ -58,7 +81,7 @@ fn bm_db32dec_into(c: &mut Criterion) {
 criterion_group!{
     name = benches;
     config = Criterion::default();
-    targets = bm_random_id, bm_hash, bm_hash2, bm_isdb32, bm_db32enc_into, bm_db32dec_into
+    targets = bm_random_id, bm_hash, bm_hash2, bm_blake2b, bm_blake2b_64k, bm_isdb32, bm_db32enc_into, bm_db32dec_into
 }
 
 criterion_main!(benches);

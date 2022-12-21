06b5fc9969ca6aef32dd9c7a6218f5a800e578a4
Jason Gerard DeRose <jasonderose@gmail.com>
2022-12-20 20:52:32 -0700

    Add zstd dep plus rough example usage in demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,8 +1,11 @@
 use std::io;
+use std::fs::File;
+use std::io::prelude::*;
 use tub::util::random_hash;
 use tub::dbase32::db32enc;
 use tub::blockchain::{BlockChain};
 use tub::base::*;
+use zstd::bulk;
 
 
 fn main() -> io::Result<()> {
@@ -11,5 +14,26 @@ fn main() -> io::Result<()> {
         bc.append(BlockType::Configure, &random_hash());
         println!("{} {}", db32enc(bc.as_payload()), bc.counter());
     }
+    let mut infile = File::open(".tub/append.tub")?;
+    let mut outfile = File::create(".tub/append.tub.zstd")?;
+    let mut buf: Vec<u8> = Vec::new();
+    buf.resize(LEAF_SIZE as usize, 0);
+    let mut i = 0;
+    let mut insize = 0;
+    let mut outsize = 0;
+    loop {
+        let size = infile.read(&mut buf)?;
+        if size == 0 {
+            break;
+        }
+        insize += size;
+        println!("{} {}", i, size);
+        i += 1;
+        buf.resize(size, 0);
+        let comp = bulk::compress(&buf, 10)?;
+        outsize += comp.len();
+        outfile.write_all(&comp)?;
+    }
+    println!("{} --> {}", insize, outsize);
     Ok(())
 }

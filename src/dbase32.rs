ae44630cac0c8f4df4926387faa53d707ddea240
Jason Gerard DeRose <jasonderose@gmail.com>
2022-12-22 20:18:46 -0700

    Very much brain dumping, gotta make this welcomingg for new devs

--- src/dbase32.rs
+++ src/dbase32.rs
@@ -12,9 +12,90 @@ Dbase32 uses an encoding table whose symbols are in ASCII/UTF-8 sorted order:
 
 This means that unlike RFC-3548 Base32 encoding, the sort-order of the encoded
 data will match the sort-order of the binary data.
+
+There are several places where Tub needs to encode an ASCII version of a TubHash
+or TubId:
+
+    1. File names used in the object store
+
+    2. Web URLs
+
+    3. Human readable display of Tub commits objects
+
+    4. Copy+Pasting your favorite TubHash all over the Internet like the cool
+       kids do
+
+We can encode these in either hex or base32.  Note that base64 is not an option
+because we do need to support crappy case-insensitive file systems on lesser
+platforms like Windows and MacOS :P  And we want to use a only a single encoding
+everywhere, no variation, simple as possible.
+
+The sort order issue is actually important, but there are other reason too:
+
+    1. We don't want the complexity of padding... this encoding only supports
+       multiples of 5 bytes (40-bits) as input to be encoded (whereas RFC-3548
+       Base32 can encode inputs of arbitrary length)
+
+    2. We don't support lower case letter alternatives... the encoding is as
+       strict and simple as possible... cuz that's how your shit should be.
+       Part of the reason Jason chose a sightly different alphabet is is to
+       disambiguate Dbase32 from RFC-3548 Base32, but also to disc
+
+    3. It is a bit of a user ergonomics issue
+
+        714ad80e791995a6bcee1f98168efb8a0cdcf744
+
+        GIIQNGUK36QJSEQDX4H7TBT6K9N84BUHRVOGCWGMIAXYQGMI
+
+        CWVR6PBQGJG9XVCWCVO37GNKXJ8JB4FVAQPU6EUENG8ASJIERAC95OIJ
+
+        0f3add49182f5138e68fc414f8f36447d8f0ac3a5701241ee48e466391320473
+
+There is a strong performance argument not to use a 280-bit hash: as long as we
+stay at or below 256-bits for the hash, we can fit two hashes in a single
+64-byte cache line.  As soon as we go over that, we're gonna take a significant
+performance hit (assuming we've properly exploited the opportunity).
+
+There also a performance benefit to a 240-hash as we can handily use those 4
+extra bytes.  An obvious thing is to use 2 of those bytes for the object type
+byte
 */
 
 
+/*
+ * To mitigate timing attacks when decoding or validating a *valid* Dbase32
+ * encoded ID, the REVERSE table is rotated to the left by *42* bytes.  Haha!
+ *
+ * This allows all valid entries to fit within a single 64-byte cache line,
+ * which means that on CPUs with a 64-byte (or larger) cache line size, cache
+ * hits and misses can't leak any information about the content of a *valid*
+ * Dbase32 ID.
+ *
+ * However, cache hits and misses can still leak information about the content
+ * of an invalid ID, as the entire table spans four 64-byte cache lines.
+ *
+ * Likewise, on CPUs with a 32-byte (or smaller) cache line size, cache hits and
+ * misses can leak information about the content of *any* ID being decoded or
+ * validated, even when it's a valid Dbase32 ID.
+ *
+ * The *42* byte left rotation was chosen because it at least balances the valid
+ * entries between two 32-byte cache lines, which makes it the least leaky
+ * possible on 32-byte chache line sized CPUs.  With the 42 byte left rotation,
+ * 16 valid entries will be in each 32-byte cache line:
+ *
+ *              3456789       ABCDEFGHI    JKLMNOPQRSTUVWXY
+ *     --------------------------------    --------------------------------
+ *     ^ 1st 32-byte cache line ^          ^ 2nd 32-byte cache line ^
+ *
+ * Note that the rotated table is only an implementation issue.  Whether you
+ * rotate your left or right by however many bytes, don't it rotate it at all
+ *
+ *
+ * FIXME: do we need to explicitly request 64 byte alignment here like in C,
+ * or is Rust cooler than that?
+ *
+ * FIXME: there is probably some kickass 
+ */
 static FORWARD: &[u8; 32] = b"3456789ABCDEFGHIJKLMNOPQRSTUVWXY";
 static REVERSE: &[u8; 256] = &[
     255,255,255,255,255,255,255,255,255,
@@ -128,7 +209,7 @@ fn check_bin_txt(bin: &[u8], txt: &[u8]) {
     || txt.len() == 0 || txt.len() % 8 != 0
     || txt.len() != bin.len() * 8 / 5
     {
-        panic!("Bad dbase32 internal call: bin.len()=={}, txt.len()=={}",
+        panic!("Bad dbase32 call: bin.len()=={}, txt.len()=={}",
             bin.len(), txt.len()
         );
     }
@@ -299,37 +380,37 @@ mod tests {
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==0, txt.len()==48")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==0, txt.len()==48")]
     fn test_empty_bin_panic() {
         check_bin_txt(&[], &[0_u8; 48]);
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==31, txt.len()==48")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==31, txt.len()==48")]
     fn test_bin_mod_5_panic() {
         check_bin_txt(&[0_u8; 31], &[0_u8; 48]);
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==30, txt.len()==0")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==30, txt.len()==0")]
     fn test_empty_txt_panic() {
         check_bin_txt(&[0_u8; 30], &[]);
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==30, txt.len()==49")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==30, txt.len()==49")]
     fn test_txt_mod_8_panic() {
         check_bin_txt(&[0_u8; 30], &[0_u8; 49]);
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==30, txt.len()==24")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==30, txt.len()==24")]
     fn test_bin_txt_mismatch_panic() {
         check_bin_txt(&[0_u8; 30], &[0_u8; 24]);
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==10, txt.len()==15")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==10, txt.len()==15")]
     fn test_db32enc_into_panic_low() {
         let bin = b"binary foo";
         let mut txt = [0_u8; 16];
@@ -341,7 +422,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==10, txt.len()==17")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==10, txt.len()==17")]
     fn test_db32enc_into_panic_high() {
         let bin = b"binary foo";
         let mut txt = [0_u8; 16];
@@ -353,7 +434,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==9, txt.len()==16")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==9, txt.len()==16")]
     fn test_db32dec_into_panic_low() {
         let txt = b"FCNPVRELI7J9FUUI";
         let mut bin = [0_u8; 10];
@@ -365,7 +446,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic (expected="Bad dbase32 internal call: bin.len()==11, txt.len()==16")]
+    #[should_panic (expected="Bad dbase32 call: bin.len()==11, txt.len()==16")]
     fn test_db32dec_into_panic_high() {
         let txt = b"FCNPVRELI7J9FUUI";
         let mut bin = [0_u8; 10];
@@ -404,8 +485,8 @@ mod tests {
     fn test_roundtrip() {
         for _ in 0..5_000 {
             let bin = random_hash();
-            let txt = super::db32enc(&bin);
-            let bin2 = super::db32dec(&txt.as_bytes()).unwrap();
+            let txt = db32enc(&bin);
+            let bin2 = db32dec(&txt.as_bytes()).unwrap();
             assert_eq!(&bin, &bin2[..]);
         }
     }

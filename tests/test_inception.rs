19c7a472db1703a2788fa8d7347e20148e7ff32f
Jason Gerard DeRose <jasonderose@gmail.com>
2023-01-08 14:59:38 -0700

    Oops, actually add tests/test_inception.rs

---
+++ tests/test_inception.rs
@@ -0,0 +1,14 @@
+use tub::base::*;
+use tub::inception::*;
+
+#[cfg(test)]
+
+
+#[test]
+fn test_leaf_hashes() {
+    let lh: LeafHashes<30> = LeafHashes::new();
+    let mut buf = Vec::new();
+    lh.serialize(&mut buf);
+    assert_eq!(buf, vec![0; 8]);
+}
+

945ab951a3a88b101a827ca279214442127a9b20
Jason Gerard DeRose <jasonderose@gmail.com>
2023-01-08 14:25:52 -0700

    Reorder mods in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 //! # Bathtub DB
 
 pub mod base;
-pub mod protocol;
-pub mod util;
-pub mod dbase32;
-pub mod importer;
-pub mod helpers;
-pub mod commands;
-pub mod dvcs;
 pub mod blockchain;
 pub mod chaos;
-pub mod tub;
+pub mod commands;
+pub mod dbase32;
+pub mod dvcs;
+pub mod helpers;
+pub mod importer;
 pub mod inception;
+pub mod protocol;
+pub mod tub;
+pub mod util;
 
